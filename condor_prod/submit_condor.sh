7f57fcbeb100e2d6985a005659831a3a4f563516
richard <>
2012-07-16 13:55:20 +0000

    Removed backticks for cleaner appearance, spelling errors.

--- condor_prod/submit_condor.sh
+++ condor_prod/submit_condor.sh
@@ -20,32 +20,33 @@ CMSSWENV=/net/hisrv0001/home/luck/CMSSW_4_4_4/
 # This is the python config which is run using cmsRun.
 # Can be relative or absolute path from submit_condor.sh
 #CONFIGFILE=runForest_44X.py
-CONFIGFILE=runForest_44X_MC.py
+CONFIGFILE=/net/hisrv0001/home/luck/CMSSW_4_4_4/src/CmsHi/HiL1Algos/prod/runForest_44X_MC.py
 
 # This is the file list of inputs to give to the python config. Shell
 # globbing is useful here.
 #DATASET=/mnt/hadoop/cms/store/user/kimy/HIMinBiasUPC/MinBias-reTracking-v1v2/*
-DATASET=/mnt/hadoop/cms/store/user/yetkin/MC_Production/HydjetDrum03/Track_v4/set[12]*
+#DATASET=/mnt/hadoop/cms/store/user/yetkin/MC_Production/HydjetDrum03/Track_v4/set[12]*
+DATASET=/mnt/hadoop/cms/store/user/yenjie/Hydjet1.8/Z2/Dijet80/tracking_v1/*
 
 # This is the folder to which the output is copied after the job
 # finishes.  The folder is created if it does not already exist.
 # WARNING: large files should not be copied to destinations beginning
 # with '/net'. Use local disks or hadoop instead.
 #DESTINATION=/mnt/hadoop/cms/store/user/luck/HiMinbias_RecHitTowers_MC_v2
-DESTINATION=/mnt/hadoop/cms/store/user/luck/test
+DESTINATION=/mnt/hadoop/cms/store/user/luck/Dijet80_trackingv1_reforest
 
 # This is the name of the output files which will be placed in the
 # destination folder. "_${jobNum}.root" is appended to this filename
 # to avoid collisions.
-OUTFILE=HiForest_MC_RecHits
+OUTFILE=HiForest_dijet80_trackingv1
 
 # The number of input files each job should handle. 
-FILESPERJOB=100
+FILESPERJOB=10
 
 # The total number of input files to use. If this number is larger
 # than the real number of files in 'DATASET', uses all of the files in
 # 'DATASET'.
-MAXFILES=1
+MAXFILES=300
 
 # Location where condor logs will be placed. Logs will have
 # '-$dateTime-$jobNum' appended to avoid collision.
@@ -54,7 +55,7 @@ LOGDIR=/net/hidsk0001/d00/scratch/luck/logs
 
 
 ########################## Create subfile ###############################
-dateTime=`date +%Y%m%d%H%M`
+dateTime=$(date +%Y%m%d%H%M)
 fileCounter=0
 jobNum=0
 inputList=()
@@ -80,7 +81,7 @@ do
 Universe = vanilla
 Initialdir = .
 Executable = exec_condor.sh
-+AccountingGroup = "group_cmshi.`whoami`"
++AccountingGroup = "group_cmshi.$(whoami)"
 Arguments =  $CMSSWENV $CONFIGFILE $DESTINATION ${OUTFILE}_${i}.root ${inputList[$i]} maxEvents=-1
 GetEnv       = True
 Input = /dev/null
@@ -106,5 +107,5 @@ condor_submit subfile
 mv subfile $LOGDIR/condor-$dateTime-$i.subfile
 done
 
-echo "Sumitted $((jobNum + 1)) jobs to Condor, $fileCounter files with $FILESPERJOB files per job."
+echo "Submitted $((jobNum + 1)) jobs to Condor, $fileCounter files with $FILESPERJOB files per job."
 
