a416086f2f1c8930fa6e5efa59c22c5149e5ce56
richard <>
2013-04-04 17:55:05 +0000

    add crab examples

--- condor_prod/read_trig_logs.sh
+++ condor_prod/read_trig_logs.sh
@@ -1,52 +1,80 @@
 #!/bin/sh
 #Author Alex Barbieri, based on material from Yongsun Kim
 
-SAMPLE="/net/hisrv0001/home/luck/CONDOR_LOGS/hlt_master_8E33v2_L1reEmul/*err"
+SAMPLE="/afs/cern.ch/user/r/richard/CMSSW_5_3_6_patch1/src/crab_0_121207_204510/res/*.stdout"
+
+# TRIGGERS="
+# HLT_PAPhoton10_CaloIdVL_v1 \
+# HLT_PAPhoton10_LooseCaloIdVL_v1 \
+# HLT_PAPhoton10_TightCaloIdVL_v1 \
+# HLT_PAPhoton10_CaloIdVL_Iso50_v1 \
+# HLT_PAPhoton10_TightCaloIdVL_Iso50_v1 \
+# HLT_PAPhoton15_CaloIdVL_v1 \
+# HLT_PAPhoton20_CaloIdVL_v1 \
+# HLT_PAPhoton20_LooseCaloIdVL_v1 \
+# HLT_PAPhoton20_TightCaloIdVL_v1 \
+# HLT_PAPhoton20_CaloIdVL_Iso50_v1 \
+# HLT_PAPhoton20_TightCaloIdVL_Iso50_v1 \
+# HLT_PAPhoton30_CaloIdVL_v1 \
+# HLT_PAPhoton30_LooseCaloIdVL_v1 \
+# HLT_PAPhoton30_TightCaloIdVL_v1 \
+# HLT_PAPhoton30_CaloIdVL_Iso50_v1 \
+# HLT_PAPhoton30_TightCaloIdVL_Iso50_v1 \
+# HLT_PAPhoton40_CaloIdVL_v1 \
+# HLT_PAPhoton40_LooseCaloIdVL_v1 \
+# HLT_PAPhoton40_TightCaloIdVL_v1 \
+# HLT_PAPhoton40_CaloIdVL_Iso50_v1 \
+# HLT_PAPhoton10_NoCaloIdVL_v1 \
+# HLT_PAPhoton20_NoCaloIdVL_v1 \
+# HLT_PAPhoton30_NoCaloIdVL_v1 \
+# HLT_PAPhoton40_NoCaloIdVL_v1 \
+# HLT_PAPhoton60_NoCaloIdVL_v1 \
+# HLT_PAPhoton10_Photon10_NoCaloIdVL_v1 \
+# HLT_PAPhoton10_Photon10_CaloIdVL_v1 \
+# HLT_PAPhoton10_Photon10_TightCaloIdVL_v1 \
+# HLT_PAPhoton10_Photon10_CaloIdVL_Iso50_v1 \
+# HLT_PAPhoton10_Photon10_TightCaloIdVL_Iso50_v1 \
+# HLT_PAPhoton15_Photon10_NoCaloIdVL_v1 \
+# HLT_PAPhoton15_Photon10_CaloIdVL_v1 \
+# HLT_PAPhoton15_Photon10_TightCaloIdVL_v1 \
+# HLT_PAPhoton15_Photon10_CaloIdVL_Iso50_v1 \
+# HLT_PAPhoton20_Photon15_NoCaloIdVL_v1 \
+# HLT_PAPhoton20_Photon15_CaloIdVL_v1 \
+# HLT_PAPhoton20_Photon15_TightCaloIdVL_v1 \
+# HLT_PAPhoton20_Photon15_CaloIdVL_Iso50_v1 \
+# HLT_PAPhoton20_Photon20_NoCaloIdVL_v1 \
+# HLT_PAPhoton20_Photon20_CaloIdVL_v1 \
+# HLT_PAPhoton20_Photon20_TightCaloIdVL_v1 \
+# HLT_PAPhoton20_Photon20_CaloIdVL_Iso50_v1 \
+# HLT_PAPhoton30_Photon30_NoCaloIdVL_v1 \
+# HLT_PAPhoton15_NoCaloIdVL_v1 \"
 
 TRIGGERS="
-HLT_PAPhoton10_CaloIdVL_v1 \
-HLT_PAPhoton10_LooseCaloIdVL_v1 \
-HLT_PAPhoton10_TightCaloIdVL_v1 \
-HLT_PAPhoton10_CaloIdVL_Iso50_v1 \
-HLT_PAPhoton10_TightCaloIdVL_Iso50_v1 \
-HLT_PAPhoton15_CaloIdVL_v1 \
-HLT_PAPhoton20_CaloIdVL_v1 \
-HLT_PAPhoton20_LooseCaloIdVL_v1 \
+HLT_PAPhoton10_TightCaloIdVL_v2 \
+HLT_PAPhoton10_TightCaloIdVL_Iso50_v2 \
+HLT_PAPhoton15_TightCaloIdVL_Iso50_v2 \
+HLT_PAPhoton15_TightCaloIdVL_v2 \
 HLT_PAPhoton20_TightCaloIdVL_v1 \
-HLT_PAPhoton20_CaloIdVL_Iso50_v1 \
 HLT_PAPhoton20_TightCaloIdVL_Iso50_v1 \
-HLT_PAPhoton30_CaloIdVL_v1 \
-HLT_PAPhoton30_LooseCaloIdVL_v1 \
 HLT_PAPhoton30_TightCaloIdVL_v1 \
-HLT_PAPhoton30_CaloIdVL_Iso50_v1 \
 HLT_PAPhoton30_TightCaloIdVL_Iso50_v1 \
-HLT_PAPhoton40_CaloIdVL_v1 \
-HLT_PAPhoton40_LooseCaloIdVL_v1 \
 HLT_PAPhoton40_TightCaloIdVL_v1 \
-HLT_PAPhoton40_CaloIdVL_Iso50_v1 \
-HLT_PAPhoton10_NoCaloIdVL_v1 \
+HLT_PAPhoton10_NoCaloIdVL_v2 \
+HLT_PAPhoton15_NoCaloIdVL_v2 \
 HLT_PAPhoton20_NoCaloIdVL_v1 \
 HLT_PAPhoton30_NoCaloIdVL_v1 \
 HLT_PAPhoton40_NoCaloIdVL_v1 \
 HLT_PAPhoton60_NoCaloIdVL_v1 \
 HLT_PAPhoton10_Photon10_NoCaloIdVL_v1 \
-HLT_PAPhoton10_Photon10_CaloIdVL_v1 \
 HLT_PAPhoton10_Photon10_TightCaloIdVL_v1 \
-HLT_PAPhoton10_Photon10_CaloIdVL_Iso50_v1 \
 HLT_PAPhoton10_Photon10_TightCaloIdVL_Iso50_v1 \
 HLT_PAPhoton15_Photon10_NoCaloIdVL_v1 \
-HLT_PAPhoton15_Photon10_CaloIdVL_v1 \
 HLT_PAPhoton15_Photon10_TightCaloIdVL_v1 \
-HLT_PAPhoton15_Photon10_CaloIdVL_Iso50_v1 \
 HLT_PAPhoton20_Photon15_NoCaloIdVL_v1 \
-HLT_PAPhoton20_Photon15_CaloIdVL_v1 \
 HLT_PAPhoton20_Photon15_TightCaloIdVL_v1 \
-HLT_PAPhoton20_Photon15_CaloIdVL_Iso50_v1 \
 HLT_PAPhoton20_Photon20_NoCaloIdVL_v1 \
-HLT_PAPhoton20_Photon20_CaloIdVL_v1 \
-HLT_PAPhoton20_Photon20_TightCaloIdVL_v1 \
-HLT_PAPhoton20_Photon20_CaloIdVL_Iso50_v1 \
-HLT_PAPhoton30_Photon30_NoCaloIdVL_v1"
+HLT_PAPhoton30_Photon30_NoCaloIdVL_v1 \
+"
 
 echo "Reading trigger logs."
 echo "#trigger #total #name"
